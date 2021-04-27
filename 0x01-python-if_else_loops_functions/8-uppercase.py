ba672c1b52998a18dfc1bbb9a0c710d56155f0b4
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-04-27 16:00:47 +0000

    Adding non-advanced task files.

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+	str2 = ""
+	for i in str:
+		if ord('a') <= ord(i) and ord(i) <= ord('z'):
+			i = ord(i) - 32
+			str2 = str2 + chr(i)
+		else:
+			str2 = str2 + i
+	print("{}".format(str2))
