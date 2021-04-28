e213fb537a6fa1253e7bfc075364da23e22ce799
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-04-28 21:42:54 +0000

    PEP8 style correction.

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,5 +3,5 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     else:
-        my_list.sort(reverse=True) #makes list descend#
+        my_list.sort(reverse=True)  # makes list max first #
         return my_list[0]
