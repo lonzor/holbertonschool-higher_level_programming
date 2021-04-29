8cabc576ac739434e6cfa688b995455ea3c7ea89
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-04-29 17:23:17 +0000

    Added default=None to max function.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        best = max(a_dictionary.values())
+        best = max(a_dictionary.values(), default=None)
         for key, value in a_dictionary.items():
             if value == best:
                 return key
