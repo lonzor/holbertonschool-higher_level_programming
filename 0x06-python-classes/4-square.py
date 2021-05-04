ba6f7be759f4b431fcaf8b3874e7a40556861500
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-04 23:09:22 +0000

    Adding file for task 4.

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Defines the class Square
+"""
+
+
+class Square:
+    """ defines class with private attribute size """
+    def __init__(self, size=0):
+        self.__size = size
+
+    """ gets size """
+    @property
+    def size(self):
+        return self.__size
+
+    """ checks int type and value >= 0 """
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    """ gets area of square """
+    def area(self):
+        return self.__size ** 2
