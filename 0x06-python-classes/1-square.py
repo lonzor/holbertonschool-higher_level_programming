aaa8c2740734968605160ce17278913a503e2361
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-04 21:08:24 +0000

    Adding file for task 1.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines the class Square
+"""
+
+
+class Square:
+    """ defines class with private attribute size """
+    def __init__(self, size):
+        self.__size = size
