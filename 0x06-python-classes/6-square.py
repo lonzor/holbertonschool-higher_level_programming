3bd753f9bfea05163f08df80e25002d09341ed1b
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-05 00:28:23 +0000

    PEP8 correction and redo of square print.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,7 +6,7 @@ Defines the class Square
 
 class Square:
     """ defines class with private attribute size """
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
 
@@ -50,15 +50,9 @@ class Square:
     """ prints square using hashtags """
     def my_print(self):
         if self.__size == 0:
-            print()
-        else:
-            pos0 = self.position[0]
-            pos1 = self.position[1]
-            for index in range(pos1):
-                print("")
-            for r in range(self.size):
-                for x in range(pos0):
-                    print(" ", end="")
-                for c in range(self.size):
-                    print("#", end="")
-                print()
+            print("")
+            return None
+        for index in range(self.position[1]):
+            print("")
+        for index in range(self.__size):
+            print("{}{}".format(" " * self.__position[0], "#" * self.__size))
