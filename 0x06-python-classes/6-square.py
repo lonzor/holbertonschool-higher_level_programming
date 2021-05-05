5441eac51bcc78aa1c58e53fbc015139be13889c
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-05 00:35:23 +0000

    Added () to value[1] checks.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,7 +42,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
+        if type(value[0]) < 0 or type(value[1]) < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
