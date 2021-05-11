6a2b31a8123c8321df7eb25d31ace71ec0cda172
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-11 14:54:57 +0000

    Adding files for task 1.

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module for class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """ defined class MyList """
+    def __init__(self):
+        """ initializes MyList """
+        pass
+    def print_sorted(self):
+        """ prints sorted list """
+        print(sorted(self))
