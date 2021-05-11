3fa2290fd32fefbb03d235160cb77e7fc19f8964
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-11 15:43:51 +0000

    Adding file for task 4.

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module for function inherits_from(obj, a_class)
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that
+    inherited directly or indirectly from the specified class.
+    """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
