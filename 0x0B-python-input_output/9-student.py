5b343e31fcbd61ea9e22d06d0a9606b61829f0ac
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-12 12:50:30 +0000

    Adding file for task 9,

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module contains the class Student.
+Instantiates object with first_name, last_name, and age.
+"""
+
+
+class Student:
+    """
+    Definition of class Student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new object from class Student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """
+        Returns dictionary description of Student object (JSON).
+        """
+        return self.__dict__
