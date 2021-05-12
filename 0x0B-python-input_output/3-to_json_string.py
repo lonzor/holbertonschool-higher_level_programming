4233a8d59bede040107ff6e3f013abef11a7bb76
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-12 11:13:25 +0000

    Adding file for task 3.

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module for to_json_string(my_obj).
+Returns the JSON representation of a string object.
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of a string object.
+    """
+    return json.dumps(my_obj)
