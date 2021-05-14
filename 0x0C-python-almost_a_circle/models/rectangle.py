b0ad4181c68f7dcd560baa999c04684489936bdf
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-14 13:17:56 +0000

    Adding code for task 3.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,7 +29,14 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        """ Setter for 'width' """
+        """
+        Setter for 'width'
+        Validating for negative value and type int
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -39,7 +46,14 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        """ Setter for 'height' """
+        """
+        Setter for 'height'
+        Validating for negative value and type int
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -49,7 +63,14 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        """ Setter for 'x' """
+        """
+        Setter for 'x'
+        Validating for negative value and type int
+        """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -59,5 +80,12 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        """ Setter for 'y' """
+        """
+        Setter for 'y'
+        Validating for negative value and type int
+        """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
