64bd245f9a6044dd9da3e2e57b26d5d2dc1019b6
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-17 01:43:23 +0000

    Adding code for task 17.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,6 +34,15 @@ class Base:
         else:
             return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation 'json_string'
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """
