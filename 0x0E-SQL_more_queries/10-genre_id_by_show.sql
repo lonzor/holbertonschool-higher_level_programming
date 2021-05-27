db1686f8a570b7ef16521b998e783f427502e82a
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-27 19:08:17 +0000

    Corrected more typos.

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_shows_genres
-ON tv_shows_genres.show_id = tv_shows.
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
