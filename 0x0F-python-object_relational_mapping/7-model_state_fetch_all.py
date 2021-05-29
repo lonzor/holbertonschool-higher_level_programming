fe3fc6ae24694b38bb8063a0a80f4e3b943013b5
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-29 23:17:38 +0000

    Added longer doc string

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Module for script uses SQLAlchemy to list all States"""
+"""
+lists all states using SQLAlchemy
+from the database hbtn_0e_0_usa
+"""
 
 
 from sys import argv
@@ -7,9 +10,10 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (argv[1], argv[2], argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
 
 Session = sessionmaker(bind=engine)
 session = Session()
