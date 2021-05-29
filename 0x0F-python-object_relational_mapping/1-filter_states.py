cb88d5557e430b5cfb52e3655ea31f3fd2de4621
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-29 20:51:40 +0000

    Added if statement for N and removed it from SQL code.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,10 +13,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
-                ORDER BY id;")
+    cur.execute("SELECT * FROM states ORDER BY states.id;")
     state_rows = cur.fetchall()
     for state in state_rows:
-        print(state)
+	if row[1].startswith("N");
+            print(state)
     cur.close()
     db.close()
