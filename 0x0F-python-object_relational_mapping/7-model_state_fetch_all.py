8bcb776327d0aff8cbb6f54c7b4f240aba163fce
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-29 23:13:36 +0000

    Not sure why it's not passing checks.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Module for script uses SQLAlchemy to list all States
-"""
+"""Module for script uses SQLAlchemy to list all States"""
 
 
 from sys import argv
@@ -9,12 +7,13 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
                            (argv[1], argv[2], argv[3]))
+
 Session = sessionmaker(bind=engine)
 session = Session()
+
 for state_inst in session.query(State).order_by(State.id):
     print("{}: {}".format(state_inst.id, state_inst.name))
 session.close()
