08b65da8f224fdae57d69f0bc78d561c5cef5406
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-29 23:21:36 +0000

    Changed code around to see if that would fix error of not picking up documentation.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,19 +5,16 @@ from the database hbtn_0e_0_usa
 """
 
 
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-
 if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-Session = sessionmaker(bind=engine)
-session = Session()
-
-for state_inst in session.query(State).order_by(State.id):
-    print("{}: {}".format(state_inst.id, state_inst.name))
-session.close()
+    for state_inst in session.query(State).order_by(State.id):
+        print("{}: {}".format(state_inst.id, state_inst.name))
+    session.close()
