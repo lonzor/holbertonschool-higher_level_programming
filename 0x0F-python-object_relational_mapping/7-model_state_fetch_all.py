26d990924b15d3ea6a0afa6ff8da3a6f05d7bc06
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-29 23:06:35 +0000

    Forgot to add argv[2] for password.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (argv[1], 'qp10wo29', argv[3]))
+                           (argv[1], argv[2], argv[3]))
 Session = sessionmaker(bind=engine)
 session = Session()
 for state_inst in session.query(State).order_by(State.id):
