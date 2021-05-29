e2b09d185442ed9733244ddd2c6e332a7b6ab3fe
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-29 21:04:11 +0000

    Trying again with LIKE BINARY. Forgot to chmod file.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,12 +11,13 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                         passwd="qp10wo29", db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id;")
+    cur.execute("SELECT * from states \
+                 WHERE name LIKE BINARY 'N%' \
+                 ORDER BY states.id ASC")
     state_rows = cur.fetchall()
     for state in state_rows:
-	if row[1].startswith("N");
-            print(state)
+        print(state)
     cur.close()
     db.close()
