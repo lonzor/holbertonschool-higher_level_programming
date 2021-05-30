c62a0cbdd7dabb5c6bf72a61dfed9def37bd68e5
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-30 00:28:29 +0000

    Adding file for task 13.

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+lists all states using SQLAlchemy
+from the database hbtn_0e_0_usa
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state_inst in session.query(State).order_by(State.id):
+        if 'a' is in state_inst.name:
+            session.delete(state_inst)
+    session.commit()
+    session.close()
+
