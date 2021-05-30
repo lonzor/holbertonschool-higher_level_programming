0dbcac8bec93fdc790c284a78de1d97daed11452
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-30 00:08:03 +0000

    Adding file for task 10.

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+lists all states using SQLAlchemy
+from the database hbtn_0e_0_usa
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_inst = session.query(State).filter(State.name == argv[4]).first()
+
+    if state_inst is None:
+        print("Not found")
+    else:
+        print(state_inst.id)
+    session.close()
