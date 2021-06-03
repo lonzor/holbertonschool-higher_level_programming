229120200daaaf8506f7c8499ece644d50a7a137
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-06-03 16:14:19 +0000

    semistandard changes.

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  function itConverts(number) {
+  function itConverts (number) {
     return number.toString(base);
   }
   return itConverts;
-}
+};
