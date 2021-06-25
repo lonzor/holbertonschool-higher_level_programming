caed837a677c175a8f9613100f666f219ffd2ad0
Lonzo Rust <rustlonzo@gmail.com>
2021-06-25 17:14:35 +0000

    Adding files for task 3 and 4.

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,4 @@
+// makes header text red when clicked
+$('div#red_header').on('click', function () {
+  $('header').addClass('red');
+});
---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,4 @@
+// changes header text color from red or green upon click
+$('div#toggle_header').on('click', function () {
+  $('header').toggleClass('red green');
+});
